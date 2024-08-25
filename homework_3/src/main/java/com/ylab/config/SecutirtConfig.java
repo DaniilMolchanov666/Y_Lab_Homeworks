7e8bcbd406cc228a092221c1aa56e334b224a80c
DaniilMolchanov666 <cottonpads1234@yandex.ru>
2024-08-25 22:55:24 +0300

    update readme

--- homework_3/src/main/java/com/ylab/config/SecutirtConfig.java
+++ homework_3/src/main/java/com/ylab/config/SecutirtConfig.java
@@ -1,6 +1,7 @@
 package com.ylab.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
@@ -10,9 +11,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/public/**").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
